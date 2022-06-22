560da3d93e0fe049257b10a95e028db5ca2bbfe2
iamrajabli <iamrajabli@outlook.com>
2022-06-22 15:41:13 +0400

    File update

--- 08Hooks/hooks/src/App.jsx
+++ 08Hooks/hooks/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, Component } from 'react'
+import { Component, useState, useEffect, useMemo } from 'react'
 import './App.css'
 
 // class Slider extends Component {
@@ -68,53 +68,90 @@ import './App.css'
 // }
 
 
-function Slider() {
-  const [slide, setSlide] = useState(0);
+// function Slider() {
+//   const [slide, setSlide] = useState(0);
 
-  const click = () => {
-    console.log('click');
-  }
-  useEffect(() => {
-    document.title = slide;
+//   const click = () => {
+//     console.log('click');
+//   }
+//   useEffect(() => {
+//     document.title = slide;
 
-    window.addEventListener('click', click)
-    return () => {
-      window.removeEventListener('click', click);
-    }
-
-
-  }, [slide])
+//     window.addEventListener('click', click)
+//     return () => {
+//       window.removeEventListener('click', click);
+//     }
 
 
-  const changeSlide = (i) => {
-    setSlide(slide => i + slide);
-  }
+//   }, [slide])
 
 
-  return (
+//   const changeSlide = (i) => {
+//     setSlide(slide => i + slide);
+//   }
 
-    <>
-      <div>
-        <h1>Number: {slide}</h1>
-        <button onClick={() => changeSlide(1)}>+</button>
-        <button onClick={() => changeSlide(-1)}>-</button> <br /><br />
-      </div>
-    </>
-  )
-}
+
+//   return (
+
+//     <>
+//       <div>
+//         <h1>Number: {slide}</h1>
+//         <button onClick={() => changeSlide(1)}>+</button>
+//         <button onClick={() => changeSlide(-1)}>-</button> <br /><br />
+//       </div>
+//     </>
+//   )
+// }
+
+
+// function App() {
+//   const [auto, setAuto] = useState(true);
+
+//   const changeAuto = () => {
+//     setAuto(auto => !auto)
+//   }
+
+//   return (
+//     <div className="App">
+//       <button onClick={changeAuto}>Show</button>
+//       {auto ? <Slider /> : null}
+//     </div>
+//   )
+// }
+
 
 
 function App() {
-  const [auto, setAuto] = useState(true);
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState('')
 
-  const changeAuto = () => {
-    setAuto(auto => !auto)
+  const changeCount = (i) => {
+    setCount(count => count + i);
+
   }
+
+  // const rend = () => {
+  //   [...new Array(10000000)].forEach((item) => { })
+
+  //   return count * 2;
+  // }
+
+  // const dublicateCount = rend();
+
+
+  const dublicateCount = useMemo(() => {
+    [...new Array(10000000)].forEach((item) => { })
+
+    return count * 2;
+  }, [count])
 
   return (
     <div className="App">
-      <button onClick={changeAuto}>Show</button>
-      {auto ? <Slider /> : null}
+      <p>{count}</p>
+      <button onClick={() => changeCount(1)}>Plus</button>
+      <p>{dublicateCount}</p>
+      <hr />
+      <input onChange={(e) => setText(e.target.value)} type="text" />
     </div>
   )
 }
